d601c025a042cf7d34ce9cf1540f9f8e7e898073
TomBagiau <bagiautom@gmail.com>
2025-11-18 19:31:51 +0100

    init logo

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,12 @@
+import tonalyLogo from "../assets/tonaly.svg";
+
+export default function Header() {
+  return (
+    <header>
+      <img
+        src={tonalyLogo}
+        alt="Tonaly logo"
+      />
+    </header>
+  );
+}
